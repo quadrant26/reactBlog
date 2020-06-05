604b6ef3a4f7e7de92bdda7ce79afac26788f003
quadrant26 <quadrant26@163.com>
2020-06-05 17:43:03 +0800

    markdown 使用

--- blog/pages/detailed.js
+++ blog/pages/detailed.js
@@ -2,9 +2,13 @@ import React from 'react'
 import Head from 'next/head'
 import {Row, Col, Breadcrumb, Affix} from 'antd'
 import {CalendarOutlined, FireOutlined, FolderOutlined } from '@ant-design/icons'
-import ReactMarkdown from 'react-markdown'
-import MarkdownNavBar from 'markdown-navbar'
-import 'markdown-navbar/dist/navbar.css';
+// import ReactMarkdown from 'react-markdown'
+// import MarkdownNavBar from 'markdown-navbar'
+// import 'markdown-navbar/dist/navbar.css';
+import marked from 'marked'
+import hljs from 'highlight.js';
+// import 'highlight.js/styles/github.css';
+import 'highlight.js/styles/monokai-sublime.css';
 
 import axios from 'axios'
 
@@ -15,42 +19,61 @@ import Footer from '../components/Footer'
 
 import '../static//style/pages/detailed.css'
 
-const Detailed = () => {
+const Detailed = (props) => {
 
-  let markdown=' \n\n\n'+'# P01:课程介绍和环境搭建\n' +
-  '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
-  '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
-   '**这是加粗的文字**\n\n' +
-  '*这是倾斜的文字*`\n\n' +
-  '***这是斜体加粗的文字***\n\n' +
-  '~~这是加删除线的文字~~ \n\n'+
-  '\`console.log(111)\` \n\n'+
-  '# p02:来个Hello World 初始Vue3.0\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n'+
-  '***\n\n\n' +
-  '# p03:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '# p04:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '#5 p05:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '# p06:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '# p07:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '``` var a=11; ```'
+  const render = new marked.Renderer();
+
+  marked.setOptions({
+    renderer: render, 
+    gfm: true,
+    pedantic: false,
+    sanitize: false,
+    tables: true,
+    breaks: false,
+    smartLists: true,
+    smartypants: false,
+    highlight: function (code) {
+      return hljs.highlightAuto(code).value;
+    }
+  });
+
+  let markedStr = `
+    \`\`\`
+      yarn add marked
+      yarn add highlight
+
+      import marked from 'marked'
+      import hljs from "highlight.js";
+      import 'highlight.js/styles/monokai-sublime.css';
+
+      const renderer = new marked.Renderer();
+
+      marked.setOptions({
+          renderer: renderer, 
+          gfm: true,
+          pedantic: false,
+          sanitize: false,
+          tables: true,
+          breaks: false,
+          smartLists: true,
+          smartypants: false,
+          highlight: function (code) {
+              return hljs.highlightAuto(code).value;
+          }
+      }); 
+
+      let html = marked(props.article_content) 
+    \`\`\`
+    + renderer: 这个是必须填写的，你可以通过自定义的Renderer渲染出自定义的格式
+    + gfm：启动类似Github样式的Markdown,填写true或者false
+    + pedatic：只解析符合Markdown定义的，不修正Markdown的错误。填写true或者false
+    + sanitize: 原始输出，忽略HTML标签，这个作为一个开发人员，一定要写flase
+    + tables： 支持Github形式的表格，必须打开gfm选项
+    + breaks: 支持Github换行符，必须打开gfm选项，填写true或者false
+    + smartLists：优化列表输出，这个填写ture之后，你的样式会好看很多，所以建议设置成ture
+    + highlight: 高亮显示规则 ，这里我们将使用highlight.js来完成`
+
+  let html = marked(markedStr) 
 
   return (
     <div className="container">
@@ -73,16 +96,16 @@ const Detailed = () => {
               <div className="detailed-title">
               青玉案·元夕</div>
               <div className="list-icon center">
-                <span><CalendarOutlined /> 2019-06-28</span>
-                <span><FolderOutlined />文章</span>
-                <span><FireOutlined /> 5498人</span>
+                <span><CalendarOutlined />{props.addTime}</span>
+                <span><FolderOutlined />{props.typeName}</span>
+                <span><FireOutlined />{props.view_count}人</span>
               </div>
-
-              <div className="detailed-content" >
-                <ReactMarkdown 
-                  source={markdown}
-                  escapeHtml={false}
-                />
+              <div className="detailed-content">
+                {props.article_cointent}
+              </div>
+              <div className="detailed-content"
+                dangerouslySetInnerHTML={{__html: html}}
+              >
               </div>
             </div>
           </div>
@@ -93,12 +116,7 @@ const Detailed = () => {
           <Affix offsetTop={5}>
             <div className="detailed-nav comm-box">
               <div className="nav-title">文章目录</div>
-              <MarkdownNavBar
-                className="article-menu"
-                source={markdown}
-                headingTopOffset={0}
-                ordered={false}
-              />
+              
             </div>
           </Affix>
         </Col>
