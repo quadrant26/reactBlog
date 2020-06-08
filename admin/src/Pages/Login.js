997a9b17d58f573c422cffa4540a132f6a5dcfd5
quadrant26 <quadrant26@163.com>
2020-06-08 16:31:21 +0800

    后端登录接口实现

--- admin/src/Pages/Login.js
+++ admin/src/Pages/Login.js
@@ -1,16 +1,49 @@
 import React, { useState } from 'react'
 import 'antd/dist/antd.css';
-import { Card, Input, Icon,Button ,Spin } from 'antd';
+import { Card, Input, Icon,Button ,Spin, message } from 'antd';
 
 import '../static/css/Login.css'
 
-function Login (){
+import axios from 'axios'
+import servicePath from '../config/servicePath'
+
+
+function Login (props){
     const [userName , setUserName] = useState('')
     const [password , setPassword] = useState('')
     const [isLoading, setIsLoading] = useState(false)
 
     const checkLogin = ()=>{
         setIsLoading(true)
+        if (!userName){
+            message.error('用户名不能为空')
+            return false
+        }
+        if (!password){
+            message.error('密码不能为空')
+            return false
+        }
+
+        let dataProps = {
+            "userName": userName,
+            "password": password
+        }
+
+        axios({
+            method: 'post',
+            url: servicePath.checkLogin,
+            data: dataProps,
+            withCredentials: true
+        }).then( (res) => {
+            setIsLoading(false)
+            if( res.data.data == "登录成功"){
+                localStorage.setItem('openId', res.data.openId)
+                props.history.push('/index')
+            }else{
+                message.error("用户名或密码错误")
+            }
+        })
+
         setTimeout(()=>{
             setIsLoading(false)
         },1000)
