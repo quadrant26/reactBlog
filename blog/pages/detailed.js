1e1ffea39db9a2e89b8c3b24343824e83534f7ec
quadrant26 <quadrant26@163.com>
2020-06-08 11:20:18 +0800

    markdown 使用

--- blog/pages/detailed.js
+++ blog/pages/detailed.js
@@ -43,51 +43,43 @@ const Detailed = (props) => {
       return hljs.highlightAuto(code).value;
     }
   });
+  let markedStr2 = `# P01:课程介绍和环境搭建\n # P02:来个Hello World 初始Vue3.0\n`;
+  let markedStr = '# P01:课程介绍和环境搭建\n' +
+  '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
+  '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
+   '**这是加粗的文字**\n\n' +
+  '*这是倾斜的文字*`\n\n' +
+  '***这是斜体加粗的文字***\n\n' +
+  '~~这是加删除线的文字~~ \n\n'+
+  '\`console.log(111)\` \n\n'+
+  '# p02:来个Hello World 初始Vue3.0\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n'+
+  '***\n\n\n' +
+  '# p03:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '# p04:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '#5 p05:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '# p06:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '# p07:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '``` var a=11; ```'
 
-  let markedStr = `
-
-    # 第一季
-
-    # 第二季
-
-    # 第三季
-    
-    \`\`\`
-      yarn add marked
-      yarn add highlight
-
-      import marked from 'marked'
-      import hljs from "highlight.js";
-      import 'highlight.js/styles/monokai-sublime.css';
-
-      const renderer = new marked.Renderer();
-
-      marked.setOptions({
-          renderer: renderer, 
-          gfm: true,
-          pedantic: false,
-          sanitize: false,
-          tables: true,
-          breaks: false,
-          smartLists: true,
-          smartypants: false,
-          highlight: function (code) {
-              return hljs.highlightAuto(code).value;
-          }
-      }); 
-
-      let html = marked(props.article_content) 
-    \`\`\`
-    + renderer: 这个是必须填写的，你可以通过自定义的Renderer渲染出自定义的格式
-    + gfm：启动类似Github样式的Markdown,填写true或者false
-    + pedatic：只解析符合Markdown定义的，不修正Markdown的错误。填写true或者false
-    + sanitize: 原始输出，忽略HTML标签，这个作为一个开发人员，一定要写flase
-    + tables： 支持Github形式的表格，必须打开gfm选项
-    + breaks: 支持Github换行符，必须打开gfm选项，填写true或者false
-    + smartLists：优化列表输出，这个填写ture之后，你的样式会好看很多，所以建议设置成ture
-    + highlight: 高亮显示规则 ，这里我们将使用highlight.js来完成`
-
-  let html = marked(markedStr) 
+  let html = marked(props.article_cointent) 
 
   return (
     <div className="container">
@@ -114,9 +106,9 @@ const Detailed = (props) => {
                 <span><FolderOutlined />{props.typeName}</span>
                 <span><FireOutlined />{props.view_count}人</span>
               </div>
-              <div className="detailed-content">
+              {/* <div className="detailed-content">
                 {props.article_cointent}
-              </div>
+              </div> */}
               <div className="detailed-content"
                 dangerouslySetInnerHTML={{__html: html}}
               >
