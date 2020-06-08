9a93e7001249057aa36b5a094bd2a700370b4768
quadrant26 <quadrant26@163.com>
2020-06-08 14:13:13 +0800

    安装 antd@3.23.3

---
+++ admin/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">Hello World</Button>
+    </div>
+  );
+}
+
+export default App;
