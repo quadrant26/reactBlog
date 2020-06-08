ac11571028f079e882d165e2a5beb7011303c346
quadrant26 <quadrant26@163.com>
2020-06-08 13:29:24 +0800

    根据文章列表

--- blog/config/servicePath.js
+++ blog/config/servicePath.js
@@ -3,6 +3,8 @@ let ipUrl = 'http://127.0.0.1:7001/default/'
 let servicePath = {
     getArticleList: ipUrl + 'getArticleList',   // 首页
     getArticleById: ipUrl + 'getArticleById/',   // 详细页
+    getTypeInfo: ipUrl + 'getTypeInfo', // 导航列表
+    getListById: ipUrl + 'getListById/', // 根据类别ID获得文章列表  
 }
 
 export default servicePath;
--- blog/pages/list.js
+++ blog/pages/list.js
@@ -9,9 +9,12 @@ import Advert from '../components//Advert'
 import Footer from '../components/Footer'
 
 import '../static/style/pages/list.css'
+import Axios from 'axios'
+import servicePath from '../config/servicePath'
 
-const MyList = () => {
+const MyList = (list) => {
 
+  // const [Mylist, setMylist] = useState(list.data)
   const [Mylist, setMylist] = useState(
     [
       {"title": "青玉案·元夕", "content": "东风夜放花千树。更吹落、星如雨。宝马雕车香满路。凤箫声动，玉壶光转，一夜鱼龙舞。 蛾儿雪柳黄金缕。笑语盈盈暗香去。众里寻他千百度。蓦然回首，那人却在，灯火阑珊处。"},
@@ -63,6 +66,17 @@ const MyList = () => {
       <Footer />
     </div>
   )
+}
+
+MyList.getInitialProps = async (context) => {
+  let id = context.query.id;
+  const promise = new Promise( (resolve, reject) => {
+    Axios(servicePath.getListById+id).then( res => {
+      resolve(res)
+    })
+  })
+
+  return await promise
 }
 
 export default MyList
